9e995fd716f5c9d56b656db2e27bef114e6243af
aderibigbeganiu <aderibigbeganiu@gmail.com>
2022-11-15 21:12:37 +0100

    update binary search

--- locate_card_binary.py
+++ locate_card_binary.py
@@ -1,19 +1,33 @@
-from jovian.pythondsa import evaluate_test_cases
+from jovian.pythondsa import evaluate_test_cases, evaluate_test_case
+
+
+def test_location(cards, query, mid):
+    mid_number = cards[mid]
+    print("mid: ", mid, ", mid_number: ", mid_number)
+    if mid_number == query:
+        if mid-1 >= 0 and cards[mid-1] == query:
+            return "left"
+        else:
+            return "found"
+    elif mid_number > query:
+        return "left"
+    else:
+        return "right"
 
 def locate_card_binary(cards, query):
     lo, hi = 0, len(cards) - 1
 
     while lo <= hi:
+        print("lo: ", lo, "hi: ", hi)
         mid = (lo + hi) // 2
+        result = test_location(cards, query, mid)
+        print("result", result)
 
-        mid_number = cards[mid]
-        print("lo: ", lo, "hi: ", hi, "mid: ", mid, "mid_number: ", mid_number)
-
-        if query == mid_number:
+        if result == "found":
             return mid
-        elif query < mid_number:
+        elif result == "left":
             hi = mid - 1
-        elif query > mid_number:
+        elif result == "right":
             lo = mid + 1
     return -1
 
